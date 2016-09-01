5d8bfe36636dd3f66f968070676a3df5dedd988c
João Victor Flores da Silva <thejvfs@ulbra.edu.br>
2016-08-31 21:27:06 -0300

    initial commit plp

---
+++ aula03/test.cs
@@ -0,0 +1,32 @@
+using System;
+class test
+{
+	public static void Main()
+	{
+	
+		int total = 0;
+		
+		total = somar (100,200);
+		
+		// string mensagem = "A some de " + numero1 + " MAIS " + numero2 + " é igual a " + total;
+		
+		Console.WriteLine(mensagem);
+		Console.ReadLine();
+	}
+	static int somar(int numero1, int numero2)
+	{
+		return numero1 + numero2;
+	}
+}
+	/* help promp
+		dir - lista o conteudo da pasta
+		cd.. - volta um nivel acima da pasta
+		cd\ vai para raiz do root c:\
+		mkdir -  cria uma nova pasta direto
+			ex mkdir nomeDaPasta
+		cls - lmpa a prompt comando
+	*******************************		
+		compilar um programa C#
+			ex.
+			c:\plp\aula03\csc.exe nomeDoAarquivo.cs					
+*/			
